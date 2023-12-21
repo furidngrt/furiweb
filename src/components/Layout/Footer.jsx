1638195cb01b7da5b21a70163a6859b51a78cabb
furi dngrt <63885192+furidngrt@users.noreply.github.com>
2023-12-21 19:14:44 +0700

    Update Footer.jsx

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <>
 
             <div className="bg-dark2 w-full p-3 py-8 text-white text-center lg:grid grid-cols-3">
-                <h6 className="text-2xl font-semibold">FURI DININGRAT</h6>
+                <h6 className="text-2xl font-semibold">FURI</h6>
                 <div className="flex justify-center">
                     <ul className="text-lg font-semibold transition-all duration-300 text-white flex">
                         <Link to="/blog">
@@ -24,15 +24,15 @@ const Footer = () => {
                         >
                             <li className="cursor-pointer hover:text-purple px-2 pb-2">Stake</li>
                         </ScrollLink>
-                        <Link to="/job">
+                        <Link to="/#">
                             <li className="cursor-pointer hover:text-purple px-2 pb-2">Jobs</li>
                         </Link>
                     </ul>
                 </div>
                 <div className="flex justify-center gap-2">
-                    <FontAwesomeIcon icon={faPaperPlane} className="transition-all duration-300 cursor-pointer bg-white text-dark hover:bg-dark hover:text-white text-sm rounded-full p-[5px]" />
-                    <FontAwesomeIcon icon={faTwitter} className="transition-all duration-300 cursor-pointer bg-white text-dark hover:bg-dark hover:text-white text-sm rounded-full p-[5px]" />
-                    <FontAwesomeIcon icon={faDiscord} className="transition-all duration-300 cursor-pointer bg-white text-dark hover:bg-dark hover:text-white text-sm rounded-full p-[5px]" />
+                      <a href="https://t.me/furidngrt"><FontAwesomeIcon icon={faPaperPlane} className="transition-all duration-300 cursor-pointer bg-white text-dark hover:bg-dark hover:text-white text-sm rounded-full p-[5px]" /></a>
+                      <a href="https://x.com/furidngrt"><FontAwesomeIcon icon={faTwitter} className="transition-all duration-300 cursor-pointer bg-white text-dark hover:bg-dark hover:text-white text-sm rounded-full p-[5px]" /></a>
+                      <a href="https://#"><FontAwesomeIcon icon={faDiscord} className="transition-all duration-300 cursor-pointer bg-white text-dark hover:bg-dark hover:text-white text-sm rounded-full p-[5px]" /></a>
                 </div>
             </div>
         </>
