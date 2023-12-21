dcd49d2af1de1614ed14cd415d7422b765142e45
furi dngrt <63885192+furidngrt@users.noreply.github.com>
2023-12-21 19:10:15 +0700

    Update Job.jsx

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -5,7 +5,7 @@ const Job = () => {
                 <div className="mb-20">
                     <h1 className="md:text-5xl text-3xl font-semibold my-3">Jobs at Furi Diningrat</h1>
                     <p>
-                        Strangelove is a core developer and implementer within the Cosmos ecosystem. Strangelove contributes to core open source software enabling Cosmos infrastructure, provides advisory and implementation services to projects launching on Cosmos, and offers a full suite of infrastructure-as-a-service products. Additionally, Strangelove is an active venture investor in the Cosmos ecosystem through their partnership with Galileo, an early-stage crypto venture firm built to support the expanding Cosmos + Celestia IBC ecosystems. Learn more at strange.love
+                        furi is a core developer and implementer within the Cosmos ecosystem. Strangelove contributes to core open source software enabling Cosmos infrastructure, provides advisory and implementation services to projects launching on Cosmos, and offers a full suite of infrastructure-as-a-service products. Additionally, Strangelove is an active venture investor in the Cosmos ecosystem through their partnership with Galileo, an early-stage crypto venture firm built to support the expanding Cosmos + Celestia IBC ecosystems. Learn more at strange.love
                     </p>
                 </div>
                 <div className="mb-20">
